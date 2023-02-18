e3cac4ba3a2c894d97647a2ffed0c983ef5803f5
shreekhand.wankhede <shreekhand.wankhede@gmail.com>
2023-02-19 00:51:45 +0530

    wrong code syntax error commit- correcting this

--- src/main/java/com/example/springbootdatajpaexample/SpringBootDataJpaExampleApplication.java
+++ src/main/java/com/example/springbootdatajpaexample/SpringBootDataJpaExampleApplication.java
@@ -4,7 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public cSpringApplication.run(SpringBootDataJpaExampleApplication.class, args);
-	}
+public class SpringBootDataJpaExampleApplication {
 
-}
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataJpaExampleApplication.class, args);
+
+ 	}
+ 
+ }
+

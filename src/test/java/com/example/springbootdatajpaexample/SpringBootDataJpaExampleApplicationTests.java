da7c2339e18aefc598427e585279bdd39391da26
shreekhand.wankhede <shreekhand.wankhede@gmail.com>
2023-02-19 00:15:35 +0530

    initial commit

---
+++ src/test/java/com/example/springbootdatajpaexample/SpringBootDataJpaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootdatajpaexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDataJpaExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
